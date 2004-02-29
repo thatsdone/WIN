10966e8a84c1416cfb5ab730784f91855ce95d34
uehira <uehira@bosai.go.jp>
2004-02-29 21:17:46 +0900

    win2raw : added option -m Version 1.1.28 released.

--- src/win2raw.c
+++ src/win2raw.c
@@ -1,5 +1,5 @@
 /*
- * $Id: win2raw.c,v 1.3 2002/07/23 06:47:40 uehira Exp $
+ * $Id: win2raw.c,v 1.4 2004/02/29 12:17:47 uehira Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -16,7 +16,7 @@
 #include "subst_func.h"
 
 static const char  rcsid[] =
-   "$Id: win2raw.c,v 1.3 2002/07/23 06:47:40 uehira Exp $";
+   "$Id: win2raw.c,v 1.4 2004/02/29 12:17:47 uehira Exp $";
 static char  *progname;
 
 static void usage(void);
@@ -24,11 +24,17 @@ int main(int, char *[]);
 
 #define NAMELEN  1025
 
+/*
+ * Divide win file into
+ *  mode 5: 1 minute file (YYMMDDhh.mm)
+ *  mode 4: 1 hour file   (YYMMDD.hh)
+ *  mode 3: 1 day file    (YYMMDD)
+ */
 int
 main(int argc, char *argv[])
 {
   FILE  *fpin, *fpraw = NULL;
-  int  c, uflag = 0, vflag = 0;
+  int  c, uflag = 0, vflag = 0, mode = 5;
   char  *rawdir, fullname[NAMELEN];
   unsigned char  *dbuf = NULL;
   WIN_blocksize  dsize;
@@ -42,8 +48,19 @@ main(int argc, char *argv[])
     progname++;
 
   /* get option */
-  while ((c = getopt(argc, argv, "huv")) != -1) {
+  while ((c = getopt(argc, argv, "m:huv")) != -1) {
     switch (c) {
+    case 'm':  /* set mode */
+      if (optarg[0] == 'm')      /* minute mode (default) */
+	mode = 5;
+      else if (optarg[0] == 'h') /* hour mode */
+	mode = 4;
+      else if (optarg[0] == 'd') /* day mode */
+	mode = 3;
+      else
+	(void)fprintf(stderr, "Invalid option: -m\n");
+	usage();
+      break;
     case 'u':  /* unlink mode */
       uflag = 1;
       break;
@@ -72,8 +89,10 @@ main(int argc, char *argv[])
     }
   }
   rawdir = argv[0];
-  if(vflag)
+  if(vflag) {
     (void)printf("ourdir: %s\n", rawdir);
+    (void)printf("mode = %d\n", mode);
+  }
 
   /* init array */
   for (i = 0; i < 5; ++i)
@@ -86,7 +105,7 @@ main(int argc, char *argv[])
       continue;
 
     /* change new file */
-    if (time_cmp(dtime, dtime_save, 5) != 0) {
+    if (time_cmp(dtime, dtime_save, mode) != 0) {
       /* first, close file */
       if (fpraw != NULL)
 	if(fclose(fpraw)) {
@@ -95,11 +114,27 @@ main(int argc, char *argv[])
 	}
 
       /* new file name */
-      if (snprintf(fullname, sizeof(fullname),
-		   "%s/%02d%02d%02d%02d.%02d", rawdir, dtime[0], dtime[1],
-		   dtime[2], dtime[3], dtime[4]) >= sizeof(fullname)) {
-	(void)fprintf(stderr, "Buffer Overrun!\n");
-	exit(1);
+      if (mode == 5) {
+	if (snprintf(fullname, sizeof(fullname),
+		     "%s/%02d%02d%02d%02d.%02d", rawdir, dtime[0], dtime[1],
+		     dtime[2], dtime[3], dtime[4]) >= sizeof(fullname)) {
+	  (void)fprintf(stderr, "Buffer Overrun!\n");
+	  exit(1);
+	}
+      } else if (mode == 4) {
+	if (snprintf(fullname, sizeof(fullname),
+		     "%s/%02d%02d%02d.%02d", rawdir, dtime[0], dtime[1],
+		     dtime[2], dtime[3]) >= sizeof(fullname)) {
+	  (void)fprintf(stderr, "Buffer Overrun!\n");
+	  exit(1);
+	}
+      } else if (mode == 3) {
+	if (snprintf(fullname, sizeof(fullname),
+		     "%s/%02d%02d%02d", rawdir, dtime[0], dtime[1],
+		     dtime[2]) >= sizeof(fullname)) {
+	  (void)fprintf(stderr, "Buffer Overrun!\n");
+	  exit(1);
+	}
       }
       if (vflag)
 	(void)printf("%s\n", fullname);
@@ -112,9 +147,9 @@ main(int argc, char *argv[])
       }
 
       /* save time stamp */
-      for (i = 0; i < 5; ++i)
+      for (i = 0; i < mode; ++i)
 	dtime_save[i] = dtime[i];
-    } /* if (time_cmp(dtime, dtime_save, 5) != 0) */
+    } /* if (time_cmp(dtime, dtime_save, mode) != 0) */
 
     /* output raw data */
     if (fwrite(dbuf, 1, dsize, fpraw) != dsize) {
@@ -154,6 +189,9 @@ usage(void)
   (void)fprintf(stderr, "%s\n", rcsid);
   (void)fprintf(stderr, "Usage : %s [options] rawdir [data]\n", progname);
   (void)fprintf(stderr, "   options: -u    : unlink input data file\n");
+  (void)fprintf(stderr, "            -m [h|d]    : set mode\n");
+  (void)fprintf(stderr, "                h: 1 hour file (YYMMDD.hh)\n");
+  (void)fprintf(stderr, "                d: 1 day file (YYMMDD)\n");
   (void)fprintf(stderr, "            -v    : verbose mode\n");
   (void)fprintf(stderr, "            -h    : print this message\n");
   exit(1);

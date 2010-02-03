9816bb3681c284427f61a459e0532b86097e2c0a
uehira <uehira@bosai.go.jp>
2010-02-03 19:09:22 +0900

    * src/raw2mon : check buffer overflow.

--- src/raw2mon.c
+++ src/raw2mon.c
@@ -1,6 +1,7 @@
-/* $Id: raw2mon.c,v 1.4.4.3.2.4 2009/08/25 04:00:15 uehira Exp $ */
+/* $Id: raw2mon.c,v 1.4.4.3.2.5 2010/02/03 10:09:22 uehira Exp $ */
 /*
   program "raw2mon.c"
+
   6/3/93,6/17/94,8/17/95 urabe
   98.6.30 FreeBSD
   99.4.19 byte-order-free
@@ -15,12 +16,7 @@
 #endif
 
 #include  <stdio.h>
-#include  <string.h>
 #include  <stdlib.h>
-#include  <sys/types.h>
-#include  <sys/fcntl.h>
-#include  <sys/ioctl.h>
-#include  <sys/stat.h>
 
 #include "winlib.h"
 
@@ -29,20 +25,22 @@
 /* #define   SR_MON    5 */
 /* #define   MAX_SR    1024 */
 
-static uint8_w wbuf[SIZE_WBUF],buf[MAXSIZE];
-
 /* prototypes */
 int main(int, char *[]);
 
 int
 main(int argc, char *argv[])
-  {
+{
+  static uint8_w  wbuf[SIZE_WBUF], buf[MAXSIZE];
 
-  while(fread(buf,1,4,stdin)==4)
-    {
-    fread(buf+4,1,mkuint4(buf)-4,stdin);
-    make_mon(buf,wbuf);
-    fwrite(wbuf,1,mkuint4(wbuf),stdout);
+  while (fread(buf, 1, 4, stdin) == 4) {
+    if (mkuint4(buf) > sizeof(buf)) {
+      (void)fprintf(stderr, "Buffer overflow. Exit!!\n");
+      exit(1);
     }
-  exit(0);
+    (void)fread(buf + 4, 1, mkuint4(buf) - 4, stdin);
+    make_mon(buf, wbuf);
+    (void)fwrite(wbuf, 1, mkuint4(wbuf), stdout);
   }
+  exit(0);
+}

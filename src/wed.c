ad8e1c9dd7d626964d2f8f6a6f69e15c36a69ae3
uehira <uehira@bosai.go.jp>
2010-09-16 18:43:07 +0900

    * src/wed : 64 bit clean.

--- src/wed.c
+++ src/wed.c
@@ -1,4 +1,4 @@
-/* $Id: wed.c,v 1.5.4.3.2.4 2009/12/18 11:33:45 uehira Exp $ */
+/* $Id: wed.c,v 1.5.4.3.2.5 2010/09/16 09:43:07 uehira Exp $ */
 /* program "wed.c"
 	"wed" edits a win format data file by time range and channles
 	6/26/91,7/13/92,3/11/93,4/20/94,8/5/94,12/8/94   urabe
@@ -10,41 +10,45 @@
         2002.8.5    ignore illegal lines in ch file
         2003.10.29 exit()->exit(0)
         2006.9.21 added comment on mklong()
+	2010.9.16 64bit clean (Uehira)
 */
 
 #ifdef HAVE_CONFIG_H
-#include        "config.h"
+#include "config.h"
 #endif
 
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<string.h>
-#include	<signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
 
-#include        "winlib.h"
+#include "winlib.h"
 
-/* #define		DEBUG		0 */
-#define		DEBUG1		0
+#define	DEBUG1 0
 
-unsigned char *buf,*outbuf;
-int leng,dec_start[6],dec_end[6],dec_now[6],nch,sysch[WIN_CHMAX];
-FILE *f_param;
+static const char  rcsid[] =
+  "$Id: wed.c,v 1.5.4.3.2.5 2010/09/16 09:43:07 uehira Exp $";
 
-/* bcd_dec(dest,sour) */
-/*      char *sour; */
-/*      int *dest; */
-/* { */
-/*    int cntr; */
-/*    for(cntr=0;cntr<6;cntr++) */
-/*      dest[cntr]=((sour[cntr]>>4)&0xf)*10+(sour[cntr]&0xf); */
-/* } */
+static uint8_w *buf=NULL,*outbuf;
+static int leng,dec_start[6],dec_end[6],dec_now[6],nch;
+static WIN_ch  sysch[WIN_CHMAX];
+static FILE *f_param;
 
+/* prototypes */
+static void wabort(void);
+static void get_one_record(void);
+static WIN_bs read_data(void);
+static WIN_bs select_ch(WIN_ch *, int, uint8_w *, uint8_w *);
+int main(int, char *[]);
+
+static void
 wabort() {exit(0);}
 
+static void
 get_one_record()
 {
    int i;
-   unsigned long  outsize;
+   size_t  outsize;
 
    for(i=0;i<6;i++) dec_end[i]=dec_start[i];
    for(i=leng-1;i>0;i--){
@@ -64,7 +68,7 @@ get_one_record()
       if(read_data()<=0){
 	 exit(1);
       }
-      bcd_dec(dec_now,(char *)buf+4);
+      bcd_dec(dec_now,buf+4);
 #if DEBUG
       fprintf(stderr,"\r%02x%02x%02x %02x%02x%02x  %5d",buf[4],buf[5],
 	      buf[6],buf[7],buf[8],buf[9],*(int *)buf);
@@ -75,14 +79,14 @@ get_one_record()
    } while(time_cmp(dec_start,dec_now,6)>0);
 
    while(1){
-      outsize=select_ch(sysch,nch,buf,outbuf);
+     outsize=(size_t)select_ch(sysch,nch,buf,outbuf);
       if(outsize>10 || leng>=0)
 	/* write one sec */
 	if(fwrite(outbuf,1,outsize,stdout)==0) exit(1);
       if(leng>=0 && time_cmp(dec_now,dec_end,6)==0) break;
       /* read one sec */
       if(read_data()<=0) break;
-      bcd_dec(dec_now,(char *)buf+4);
+      bcd_dec(dec_now,buf+4);
       if(leng>=0 && time_cmp(dec_now,dec_end,6)>0) break;
 #if DEBUG
       fprintf(stderr,"\rnow  :%02x%02x%02x %02x%02x%02x",buf[4],buf[5],
@@ -95,63 +99,48 @@ get_one_record()
 #endif
 }
 
+static WIN_bs
 read_data()
 {
-   static unsigned int size;
-   int re;
-   unsigned char  tmpa[4];
+   static size_t size;
+   WIN_bs re;
+   uint8_w  tmpa[4];
  
-   /* if(fread(&re,1,4,stdin)==0) return 0; */
-   /* i=1;if(*(char *)&i) SWAPL(re); */
-   if(fread(tmpa,1,4,stdin)==0) return 0;
+   if(fread(tmpa,1,4,stdin)==0) return (0);
    re = mkuint4(tmpa);
-   if(buf==0){
-      buf=(unsigned char *)malloc(size=re*2);
-      outbuf=(unsigned char *)malloc(size=re*2);
+   if(buf==NULL){
+      buf=(uint8_w *)malloc(size=re*2);
+      outbuf=(uint8_w *)malloc(size=re*2);
    }
    else if(re>size){
-      buf=(unsigned char *)realloc(buf,size=re*2);
-      outbuf=(unsigned char *)realloc(outbuf,size=re*2);
+      buf=(uint8_w *)realloc(buf,size=re*2);
+      outbuf=(uint8_w *)realloc(outbuf,size=re*2);
    }
-   /*  *(int *)buf=re; */
    memcpy(buf, tmpa, 4);
    re=fread(buf+4,1,re-4,stdin);
-   return re;
+   return (re);
 }
 
-select_ch(sys_ch,n_ch,old_buf,new_buf)
-     unsigned char *old_buf,*new_buf;
-     int *sys_ch,n_ch;
+static WIN_bs
+select_ch(WIN_ch *sys_ch, int n_ch, uint8_w *old_buf, uint8_w *new_buf)
 {
-   int i,j,size,gsize,new_size,sr;
-   unsigned char *ptr,*new_ptr,*ptr_lim;
-   unsigned char gh[5];
- 
-   size=mkuint4(old_buf);
-   ptr_lim=old_buf+size;
+   int i,j;
+   uint32_w  gsize;
+   WIN_bs  new_size;
+   uint8_w *ptr,*new_ptr,*ptr_lim;
+   WIN_ch  chtmp;
+
+   ptr_lim=old_buf+mkuint4(old_buf);
    ptr=old_buf+4;
    new_ptr=new_buf+4;
    for(i=0;i<6;i++) *new_ptr++=(*ptr++);
    new_size=10;
    do{
-      for(i=0;i<5;i++) gh[i]=ptr[i];
-      i=((((long)gh[0])<<8)+gh[1])&0x0000ffff;
+     gsize = get_sysch(ptr, &chtmp);
 #if DEBUG1
-      fprintf(stderr,"i=%04X\n", i);
+     fprintf(stderr,"chtmp=%04hX\n", chtmp);
 #endif
-      /* sampling rate */
-      if((gh[2]&0x80)==0x0) /* channel header = 4 byte */
-	sr=gh[3]+(((long)(gh[2]&0x0f))<<8);
-      else                  /* channel header = 5 byte */
-	sr=gh[4]+(((long)gh[3])<<8)+(((long)(gh[2]&0x0f))<<16);
-      /* size */
-      if((gh[2]>>4)&0x7)
-	gsize=((gh[2]>>4)&0x7)*(sr-1)+8;
-      else
-	gsize=(sr>>1)+8;
-      if(gh[2]&0x80)
-	gsize++;
-      for(j=0;j<n_ch;j++) if(i==sys_ch[j]) break;
+      for(j=0;j<n_ch;j++) if(chtmp==sys_ch[j]) break;
       if(n_ch<0 || j<n_ch){
 	 new_size+=gsize;
 	 while(gsize-->0) *new_ptr++=(*ptr++);
@@ -162,19 +151,21 @@ select_ch(sys_ch,n_ch,old_buf,new_buf)
    new_buf[1]=new_size>>16;
    new_buf[2]=new_size>>8;
    new_buf[3]=new_size;
-   return new_size;
+   return (new_size);
 }
 
-main(argc,argv)
-     int argc;
-     char *argv[];
+int
+main(int argc, char *argv[])
 {
    char tb[100];
+   int  k;
 
    signal(SIGINT,(void *)wabort);
    signal(SIGTERM,(void *)wabort);
    
    if(argc<4){
+      WIN_version();
+      fprintf(stderr, "%s\n", rcsid);
       fprintf(stderr," usage of 'wed' :\n");
       fprintf(stderr,"   'wed [YYMMDD] [hhmmss] [len(s)] ([ch list file]) <[in_file] >[out_file]'\n");
       fprintf(stderr," example of channel list file :\n");
@@ -188,11 +179,12 @@ main(argc,argv)
 	 exit(1);
       }
       nch=0;
-      while(fscanf(f_param,"%s",tb)!=EOF)
+      while(fscanf(f_param,"%99s",tb)!=EOF)
         {
-        if(sscanf(tb,"%x",&sysch[nch])!=1) continue;
+        if(sscanf(tb,"%x",&k)!=1) continue;
+	sysch[nch] = k & 0xffff;
 #if DEBUG
-	fprintf(stderr," %04X",sysch[nch]);
+	fprintf(stderr," %04hX",sysch[nch]);
 #endif
         nch++;
         }

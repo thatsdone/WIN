05eca8e615b4e07fc13b33664ce6f71d250efbf7
uehira <uehira@bosai.go.jp>
2010-09-20 16:38:37 +0900

    * src/finalb2seis.c : 64bit check.

--- src/finalb2seis.c
+++ src/finalb2seis.c
@@ -1,4 +1,4 @@
-/* $Id: finalb2seis.c,v 1.3.4.1.2.1 2010/06/18 09:50:11 uehira Exp $ */
+/* $Id: finalb2seis.c,v 1.3.4.1.2.2 2010/09/20 07:38:37 uehira Exp $ */
 /******************************************************************/
 /*    finalb2seis.c                8/19/92-6/1/93 urabe           */
 /*    How to use (for example),                                   */
@@ -11,16 +11,23 @@
 #endif
 
 #include  <stdio.h>
+#include  <stdlib.h>
+#include  <string.h>
 
 #include "winlib.h"
 
-main(argc,argv)
-  int argc;
-  char *argv[];
+static const char rcsid[] =
+   "$Id: finalb2seis.c,v 1.3.4.1.2.2 2010/09/20 07:38:37 uehira Exp $";
+
+/* prototypes */
+int main(int, char *[]);
+
+int
+main(int argc, char *argv[])
   {
   char fname[100],fname1[100];
   int i,j,no,start,year;
-  unsigned char b[16];
+  uint8_w  b[16];
   FILE *fp;
   /* struct { */
   /*   char time[8]; /\* Y,M,D,h,m,s,s10,mag10 *\/ */
@@ -31,6 +38,8 @@ main(argc,argv)
 
   if(argc<2)
     {
+    WIN_version();
+    fprintf(stderr, "%s\n", rcsid);
     fprintf(stderr,"usage of finalb2seis :\n");
     fprintf(stderr," (ex.) finalb2seis /dat/seis/eri (ogino) < [finalb file]\n");
     exit(1);
@@ -42,9 +51,18 @@ main(argc,argv)
     if(argc>2 && strncmp(argv[2],d.owner,4)) continue;
     if(start)
       {
-      sprintf(fname,"%s-%d.b",argv[1],d.time[0]);
+	if (snprintf(fname,sizeof(fname),"%s-%d.b",argv[1],d.time[0])
+	    >= sizeof(fname))
+	  {
+	    fprintf(stderr, "Buffer overrun!\n");
+	    exit(1);
+	  }
       strcpy(fname1,fname);
-      fp=fopen(fname,"w+");
+      if ((fp=fopen(fname,"w+")) == NULL)
+	{
+	  perror("fopen");
+	  exit(1);
+	}
       b[0]=b[1]=b[2]=b[3]=0;
       fwrite(b,1,4,fp);
       j=start=0;
@@ -55,9 +73,18 @@ main(argc,argv)
       b[0]=b[1]=0xff;
       fwrite(b,1,2,fp);
       fclose(fp);
-      sprintf(fname,"%s-%d.b",argv[1],d.time[0]);
+      if (snprintf(fname,sizeof(fname),"%s-%d.b",argv[1],d.time[0])
+	    >= sizeof(fname))
+	  {
+	    fprintf(stderr, "Buffer overrun!\n");
+	    exit(1);
+	  }
       strcpy(fname1,fname);
-      fp=fopen(fname,"w+");
+      if ((fp=fopen(fname,"w+")) == NULL)
+	{
+	  perror("fopen");
+	  exit(1);
+	}
       b[0]=b[1]=b[2]=b[3]=0;
       fwrite(b,1,4,fp);
       j=0;
@@ -71,8 +98,17 @@ main(argc,argv)
       b[1]=0xff;
       fwrite(b,1,2,fp);
       fclose(fp);
-      sprintf(fname+strlen(fname1),"%d",no);
-      fp=fopen(fname,"w+");
+      if (snprintf(fname+strlen(fname1),sizeof(fname)-strlen(fname1),"%d",no)
+	  >= sizeof(fname)-strlen(fname1))
+	{
+	  fprintf(stderr, "Buffer overrun!\n");
+	  exit(1);
+	}
+      if ((fp=fopen(fname,"w+")) == NULL)
+	{
+	  perror("fopen");
+	  exit(1);
+	}
       b[0]=0;
       b[1]=0;
       b[2]=0;
@@ -86,24 +122,24 @@ main(argc,argv)
     b[3]=d.time[3]; /* hour */
     b[4]=d.time[4]; /* min */
     i=d.time[5]*10+d.time[6];
-    b[5]=i%256;
-    b[6]=i/256;
+    b[5]=(uint8_w)(i%256);
+    b[6]=(uint8_w)(i/256);
     i=1;if(*(char *)&i)
       {
       SWAPF(d.alat);  
       SWAPF(d.along);
       SWAPF(d.dep);
       }
-    b[7]=(int)d.along;
-    b[8]=(int)((d.along-(float)b[7])*60.0);
-    b[9]=(int)((d.along-(float)b[7]-((float)b[8])/60.0)*3600.0+0.5);
-    b[10]=(int)d.alat;
-    b[11]=(int)((d.alat-(float)b[10])*60.0);
-    b[12]=(int)((d.alat-(float)b[10]-((float)b[11])/60.0)*3600.0+0.5);
+    b[7]=(uint8_w)d.along;
+    b[8]=(uint8_w)((d.along-(float)b[7])*60.0);
+    b[9]=(uint8_w)((d.along-(float)b[7]-((float)b[8])/60.0)*3600.0+0.5);
+    b[10]=(uint8_w)d.alat;
+    b[11]=(uint8_w)((d.alat-(float)b[10])*60.0);
+    b[12]=(uint8_w)((d.alat-(float)b[10]-((float)b[11])/60.0)*3600.0+0.5);
     if(d.dep<0.0) d.dep=0.0;    /**************/
     i=(int)(d.dep*10.0+0.5);
-    b[13]=i%256;
-    b[14]=i/256;
+    b[13]=(uint8_w)(i%256);
+    b[14]=(uint8_w)(i/256);
     if(d.time[7]==99) b[15]=0;    /**************/
     else if(d.time[7]>0) b[15]=d.time[7];
     else b[15]=0x80-d.time[7];    /* 5/25/93 */
@@ -116,4 +152,6 @@ main(argc,argv)
     fwrite(b,1,2,fp);
     fclose(fp);
     }
+
+  exit(0);
   }

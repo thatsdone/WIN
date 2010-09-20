81367ba75a42a8baa08a5c2a9dcc4ec8474140c0
uehira <uehira@bosai.go.jp>
2010-09-20 17:35:59 +0900

    * src/pick2finalb.c : 64bit check.

--- src/pick2finalb.c
+++ src/pick2finalb.c
@@ -1,4 +1,4 @@
-/* $Id: pick2finalb.c,v 1.4.4.2.2.2 2010/09/20 08:24:02 uehira Exp $ */
+/* $Id: pick2finalb.c,v 1.4.4.2.2.3 2010/09/20 08:35:59 uehira Exp $ */
 /* pick2finalb.c */
 /* 8/22/91, 5/22/92, 7/9/92, 8/19/92, 5/25/93, 6/1/93 urabe */
 /* 97.10.3 FreeBSD */
@@ -13,11 +13,19 @@
 #endif
 
 #include  <stdio.h>
+#include  <stdlib.h>
 #include  <string.h>
 #include  <ctype.h>
 
 #include "winlib.h"
 
+static const char rcsid[] =
+   "$Id: pick2finalb.c,v 1.4.4.2.2.3 2010/09/20 08:35:59 uehira Exp $";
+
+/* prototypes */
+int main(void);
+
+int
 main()
   {
   FILE *fp;
@@ -25,7 +33,6 @@ main()
   char tbuf[1024],fname[256],buf[1024],owner[20],diag[256],diagsave[256],
     item[256],tb[10][256];
   char *ptr;
-  unsigned int ye,mo,da,ho,mi;
   int i,tm[6],tmc[7];
   double se,sec,mag;
   /* struct { */
@@ -35,7 +42,7 @@ main()
   /*   } d; */
   struct FinalB  d;      /* 28 bytes / event */
 
-  while(fgets(tbuf,sizeof(tbuf),stdin))
+  while(fgets(tbuf,sizeof(tbuf),stdin) != NULL)
     {
       i=sscanf(tbuf,"%255s%255s%255s%255s%255s%255s%255s%255s%255s%255s",
 	       tb[0],tb[1],tb[2],tb[3],tb[4],tb[5],tb[6],tb[7],tb[8],tb[9]);
@@ -43,7 +50,7 @@ main()
     strcpy(fname,tb[i-1]);
     if((fp=fopen(fname,"r"))==NULL) continue;
     flag=1;
-    while(fgets(buf,sizeof(buf),fp))
+    while(fgets(buf,sizeof(buf),fp) != NULL)
       {
       if(flag && strncmp(buf,"#p",2)==0)
         {
@@ -61,14 +68,14 @@ main()
           &tm[0],&tm[1],&tm[2],&tm[3],&tm[4],&se,
           &d.alat,&d.along,&d.dep,&mag);
         adj_sec(tm,&se,tmc,&sec);
-        d.time[0]=(unsigned char)tmc[0];
-        d.time[1]=(unsigned char)tmc[1];
-        d.time[2]=(unsigned char)tmc[2];
-        d.time[3]=(unsigned char)tmc[3];
-        d.time[4]=(unsigned char)tmc[4];
-        d.time[5]=(unsigned char)((int)sec);
-        d.time[6]=(unsigned char)(((int)(sec*10.0))%10);
-        d.time[7]=(int)(mag*10.0+0.5);
+        d.time[0]=(int8_w)tmc[0];
+        d.time[1]=(int8_w)tmc[1];
+        d.time[2]=(int8_w)tmc[2];
+        d.time[3]=(int8_w)tmc[3];
+        d.time[4]=(int8_w)tmc[4];
+        d.time[5]=(int8_w)((int)sec);
+        d.time[6]=(int8_w)(((int)(sec*10.0))%10);
+        d.time[7]=(int8_w)((int)(mag*10.0+0.5));
         *d.diag=(*d.owner)=0;
         if(*diagsave) strncpy(d.diag,diagsave,4);
         if(*owner) strncpy(d.owner,owner,4);
@@ -84,4 +91,5 @@ main()
       }
     fclose(fp);
     }
+  exit(0);
   }

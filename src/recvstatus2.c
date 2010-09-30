a1906c85de12fdd0fdc87c3b0ff238ebc0227a3b
uehira <uehira@bosai.go.jp>
2010-09-30 22:24:59 +0900

    * src/recvstatus2.c : added daemon mode. 64bit check.

--- src/recvstatus2.c
+++ src/recvstatus2.c
@@ -1,12 +1,23 @@
-/* $Id: recvstatus2.c,v 1.6.8.7 2010/09/29 16:06:34 uehira Exp $ */
+/* $Id: recvstatus2.c,v 1.6.8.8 2010/09/30 13:24:59 uehira Exp $ */
 
 /* modified from "recvstatus.c" */
 /* 2002.6.19 recvstatus2 receive A8/A9 packets from Datamark LS-7000XT */
 /* 2002.7.3 fixed a bug - 'ok' deleted */
 /* 2002.7.11 DEBUG(2) */
+/* 2010.9.30 daemon mode. 64bit check. */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
+#endif
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#if HAVE_ARPA_INET_H
+#include <arpa/inet.h>
 #endif
 
 #include <stdio.h>
@@ -14,9 +25,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <netdb.h>
+#include <errno.h>
+#include <syslog.h>
 
 #if HAVE_DIRENT_H
 # include <dirent.h>
@@ -46,14 +57,7 @@
 #endif  /* !HAVE_SYS_TIME_H */
 #endif  /* !TIME_WITH_SYS_TIME */
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#if HAVE_ARPA_INET_H
-#include <arpa/inet.h>
-#endif
-#include <netdb.h>
-#include <errno.h>
-
+#include "daemon_mode.h"
 #include "winlib.h"
 #include "udpu.h"
 
@@ -61,25 +65,44 @@
 #define MAXMESG   2048
 /* #define DEBUG   0 */
 
-static int sock;     /* socket */
+static const char rcsid[] =
+  "$Id: recvstatus2.c,v 1.6.8.8 2010/09/30 13:24:59 uehira Exp $";
 
 char *progname, *logfile = NULL;
 int syslog_mode = 0, exit_status = EXIT_SUCCESS;
 
-main(argc,argv)
-  int argc;
-  char *argv[];
+static int sock;     /* socket */
+static int daemon_mode;
+
+/* prototypes */
+static void usage(void);
+int main(int, char *[]);
+
+static void
+usage()
+{
+
+  WIN_version();
+  (void)fprintf(stderr, "%s\n", rcsid);
+  if (daemon_mode)
+    (void)fprintf(stderr, " usage : '%s (-r) [port] ([log dir])'\n", progname);
+  else
+    (void)fprintf(stderr, " usage : '%s (-Dr) [port] ([log dir])'\n", progname);
+}
+
+int
+main(int argc, char *argv[])
   {
-  unsigned char rbuf[MAXMESG];
-  char tb[100],logdir[256],logxml[256];
-  int i,fromlen,n,ns,c,rcs;
-/*   extern int optind; */
-/*   extern char *optarg; */
+  uint8_w rbuf[MAXMESG];
+  char tb[100],*logdir,logxml[256];
+  int i,ns,c,rcs;
+  ssize_t  n;
+  socklen_t  fromlen;
   struct sockaddr_in  from_addr;
-  unsigned short to_port;
+  uint16_t to_port;
   struct infoarray {
-    int adrs;
-    int port;
+    in_addr_t adrs;
+    in_port_t port;
     int id;
     char t[6];
     int ch;
@@ -93,40 +116,56 @@ main(argc,argv)
 
   if((progname=strrchr(argv[0],'/')) != NULL) progname++;
   else progname=argv[0];
-  sprintf(tb," usage : '%s (-r) [port] ([log dir])'",progname);
+
+  exit_status = EXIT_SUCCESS;
+  daemon_mode = syslog_mode = 0;
+  if (strcmp(progname, "recvstatus2d") == 0)
+    daemon_mode = 1;
+
   rcs=0;
-  while((c=getopt(argc,argv,"r"))!=-1)
+  while((c=getopt(argc,argv,"Dr"))!=-1)
     {
     switch(c)
       {
+      case 'D':
+	daemon_mode = 1;  /* daemon mode */
+	break;
       case 'r':   /* do rcs check-in */
         rcs=1;
         break;
       default:
         fprintf(stderr," option -%c unknown\n",c);
-        fprintf(stderr,"%s\n",tb);
+	usage();
         exit(1);
       }
     }
+
   optind--;
   if(argc<2+optind)
     {
-    fprintf(stderr,"%s\n",tb);
+    usage();
     exit(1);
     }
 
   to_port=atoi(argv[1+optind]);
   if(argc>2+optind)
     {
-    strcpy(logdir,argv[2+optind]);
+    logdir = argv[2+optind];
     if((dir_ptr=opendir(logdir))==NULL) err_sys("opendir");
+    if (closedir(dir_ptr)) err_sys("closedir");
     }
-  else *logdir=0;
+  else logdir=NULL;
 
-  snprintf(tb,sizeof(tb),"started. port=%d logdir=%s",to_port,logfile);
+  if (daemon_mode) {
+    syslog_mode = 1;
+    daemon_init(progname, LOG_USER, syslog_mode);
+    umask(022);
+  }
+
+  snprintf(tb,sizeof(tb),"started. port=%d logdir=%s",to_port,logdir);
   write_log(tb);
 
-  sock = udp_accept4(to_port, 32);
+  sock = udp_accept4(to_port, DEFAULT_RCVBUF);
   /* if((sock=socket(AF_INET,SOCK_DGRAM,0))<0) err_sys("socket"); */
 
   /* memset((char *)&to_addr,0,sizeof(to_addr)); */
@@ -146,7 +185,7 @@ main(argc,argv)
     fromlen=sizeof(from_addr);
     n=recvfrom(sock,rbuf,MAXMESG,0,(struct sockaddr *)&from_addr,&fromlen);
 #if DEBUG
-printf("n=%d from %s:%d\n",n,inet_ntoa(from_addr.sin_addr),ntohs(from_addr.sin_port));
+printf("n=%zd from %s:%d\n",n,inet_ntoa(from_addr.sin_addr),ntohs(from_addr.sin_port));
 for(i=0;i<25;i++) printf(" %02X",rbuf[i]);
 printf("\n");
 #endif
@@ -166,7 +205,7 @@ printf("ns=%d i=%d\n",ns,i);
           for(i=0;i<ns;i++) free(s[i]);
           i=ns=0;
           } 
-        s[i]=malloc(sizeof(struct infoarray));
+        s[i]=(struct infoarray *)malloc(sizeof(struct infoarray));
         s[i]->adrs=from_addr.sin_addr.s_addr; 
         s[i]->port=from_addr.sin_port; 
         s[i]->seq=1;
@@ -197,20 +236,27 @@ printf("s[i]->seq=%d s[i]->len=%d\n",s[i]->seq,s[i]->len);
 #if DEBUG
 printf("%s",s[i]->c);
 #endif
-        if(*logdir)
+        if(logdir != NULL)
           {
-          if(rbuf[2]==0xA8) sprintf(logxml,"%s/S%04X.xml",logdir,s[i]->ch);
-          else sprintf(logxml,"%s/M%04X.xml",logdir,s[i]->ch);
+	  if(rbuf[2]==0xA8) {
+	    if (snprintf(logxml,sizeof(logxml),
+			 "%s/S%04X.xml",logdir,s[i]->ch) >= sizeof(logxml))
+	      err_sys("snprintf");
+	  }
+	  else {
+	    if (snprintf(logxml,sizeof(logxml),
+			 "%s/M%04X.xml",logdir,s[i]->ch) >= sizeof(logxml))
+	      err_sys("snprintf");
+	  }
           if((fp=fopen(logxml,"w+"))==NULL)
-            {
-            fprintf(stderr,"file '%s' not open !\n",tb);
-            end_program();
-            }
+	    err_sys(logxml);
           fwrite(s[i]->c,1,s[i]->len+1,fp);
           fclose(fp);
           if(rcs)
             {
-            sprintf(tb,"ci -l -q %s</dev/null",logxml);
+	    if (snprintf(tb,sizeof(tb),
+			 "ci -l -q %s</dev/null",logxml) >= sizeof(tb))
+	      err_sys("snprintf");
             system(tb);
             }
           }

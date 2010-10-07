9996fcdd33ca85c87df011ceefbf5e50fdacb3ef
uehira <uehira@bosai.go.jp>
2010-10-08 00:45:58 +0900

    * src/winrandsimu6.c : 64bit clean?

--- src/winrandsimu6.c
+++ src/winrandsimu6.c
@@ -1,17 +1,24 @@
-/* $Id: winrandsimu6.c,v 1.1.4.4 2010/09/30 03:01:12 uehira Exp $ */
+/* $Id: winrandsimu6.c,v 1.1.4.5 2010/10/07 15:45:58 uehira Exp $ */
 
 /*  WIN random simulater, real-time version
      write 100Hz 3000ch data to stdout as WIN-text or shared memory
+
+     2010/10/07  64bit? (Uehira)
      
 */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <unistd.h>
 
 #if TIME_WITH_SYS_TIME
 #include <sys/time.h>
@@ -24,11 +31,7 @@
 #endif  /* !HAVE_SYS_TIME_H */
 #endif  /* !TIME_WITH_SYS_TIME */
 
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-
-#include "subst_func.h"
+#include "winlib.h"
 
 #define MAX_AMP 128
 #define EVENT   60
@@ -48,25 +51,31 @@
 
 #define USE_RND_DELAY
 
-key_t shmkey_out;
-struct Shm {
-  unsigned long p;        /* write point */
-  unsigned long pl;       /* write limit */
-  unsigned long r;        /* Latest */
-  unsigned long c;        /* counter */
-  unsigned char d[1];     /* data buffer */
-} *shm_out;
-int shmid_out, shm_size=0;
-unsigned char *ptw;
+static const char rcsid[] =
+  "$Id: winrandsimu6.c,v 1.1.4.5 2010/10/07 15:45:58 uehira Exp $";
 
-int winform(long *,unsigned char *,int,unsigned short);
+static key_t shmkey_out;
+static struct Shm *shm_out;
+static size_t shm_size=0;
+static uint8_w *ptw;
 
-double sinc(double x){
+/* prototypes */
+static double sinc(double);
+static int prt_now(int);
+static int out_data(time_t, int, int, int, int, int, int *, int *, int, int *);
+int main(int, char *[]);
+
+static double
+sinc(double x)
+{
+
   if(fabs(x)<1.0e-8) return(1.);
   else return(sin(M_PI*x)/(M_PI*x));
 }
 
-int prt_now(int type){
+static int
+prt_now(int type)
+{
   time_t tt;
 
   tt=time(NULL);
@@ -75,11 +84,12 @@ int prt_now(int type){
   return(0);
 }
 
-int main(int argc, char **argv){
+int
+main(int argc, char *argv[])
+{
   int samp, nch, sch;
-  int i, j, k, l, p, chid, dpoint[3];
+  int j, k, l, dpoint[3];
   int *ev, *de;
-  double r;
   time_t tnew, told;
 #ifdef DEBUG
   int count;
@@ -89,13 +99,15 @@ int main(int argc, char **argv){
   prt_now(1);
 #endif
   if(argc<3){
+    WIN_version();
+    fprintf(stderr, "%s\n", rcsid);
     fprintf(stderr,"Usage: %s shm_key shm_size\n",*argv);
     exit(0);
   }
 
   samp=SAMP;
   shmkey_out=atoi(*++argv);
-  shm_size=atoi(*++argv);
+  shm_size=atol(*++argv);
   shm_size=shm_size*1000;
   nch=NCH; sch=STACH;
 
@@ -111,14 +123,17 @@ int main(int argc, char **argv){
   }
 
   if(shm_size>0){
-    if((shmid_out=shmget(shmkey_out, shm_size, IPC_CREAT | 0666))<0)
-      fprintf(stderr, "error shmget_out\n");
-    if((shm_out=(struct Shm *)shmat(shmid_out,(void *)0,0))==(struct Shm *)-1)
-      fprintf(stderr, "error shmget_out\n");
+    shm_out = Shm_create_offline(shmkey_out, shm_size);
+    /* if((shmid_out=shmget(shmkey_out, shm_size, IPC_CREAT | 0666))<0) */
+    /*   fprintf(stderr, "error shmget_out\n"); */
+    /* if((shm_out=(struct Shm *)shmat(shmid_out,(void *)0,0))==(struct Shm *)-1) */
+    /*   fprintf(stderr, "error shmget_out\n"); */
+
     /* initialize output buffer */
-    shm_out->p = shm_out->c = 0;
-    shm_out->pl = (shm_size - sizeof(*shm_out)) / 10 * 9;
-    shm_out->r = -1;
+    Shm_init(shm_out, shm_size);
+    /* shm_out->p = shm_out->c = 0; */
+    /* shm_out->pl = (shm_size - sizeof(*shm_out)) / 10 * 9; */
+    /* shm_out->r = -1; */
     ptw = shm_out->d;
   }
 
@@ -179,12 +194,16 @@ int main(int argc, char **argv){
   exit(0);
 }
 
-int out_data(time_t tnow,int del,int sch,int sta,int nch,int samp,int *ev,int *de,int l,int *dpoint){
+static int
+out_data(time_t tnow, int del, int sch, int sta, int nch, int samp,
+	 int *ev, int *de, int l, int *dpoint)
+{
   int i,j,p,tm[6];
   double r;
-  static unsigned char outbuf[MAXCH][4*4*SR],tt[6],cbuf;
-  static long inbuf[SR];
-  int ii, size, chsize[MAXCH], t[6];
+  static uint8_w outbuf[MAXCH][4*4*SR],tt[6],cbuf;
+  static int32_w inbuf[SR];
+  int ii, t[6];
+  WIN_bs size, chsize[MAXCH];
   char buf[18];
   struct tm *tn;
   time_t ttime;
@@ -252,103 +271,4 @@ int out_data(time_t tnow,int del,int sch,int sta,int nch,int samp,int *ev,int *d
   }
 
   return(0);
-}
-
-/*
-   win text format to win format
-   2003.06.22- (C) Hiroshi TSURUOKA / All Rights Reserved.
-        06.30  bug fix & add fflush
-   2004.10.10  writing to share memory option
- */
-/* $Id: winrandsimu6.c,v 1.1.4.4 2010/09/30 03:01:12 uehira Exp $ */
-/* winform.c  4/30/91,99.4.19   urabe */
-/* winform converts fixed-sample-size-data into win's format */
-/* winform returns the length in bytes of output data */
-
-int winform(inbuf, outbuf, sr, sys_ch)
-long *inbuf;                    /* input data array for one sec */
-unsigned char *outbuf;          /* output data array for one sec */
-int sr;                         /* n of data (i.e. sampling rate) */
-unsigned short sys_ch;          /* 16 bit long channel ID number */
-{
-    int dmin, dmax, aa, bb, br, i, byte_leng;
-    long *ptr;
-    unsigned char *buf;
-
-    /* differentiate and obtain min and max */
-    ptr = inbuf;
-    bb = (*ptr++);
-    dmax = dmin = 0;
-    for (i = 1; i < sr; i++) {
-        aa = (*ptr);
-        *ptr++ = br = aa - bb;
-        bb = aa;
-        if (br > dmax)
-            dmax = br;
-        else if (br < dmin)
-            dmin = br;
-    }
-
-    /* determine sample size */
-    if (((dmin & 0xfffffff8) == 0xfffffff8 || (dmin & 0xfffffff8) == 0) &&
-        ((dmax & 0xfffffff8) == 0xfffffff8 || (dmax & 0xfffffff8) == 0))
-        byte_leng = 0;
-    else if (((dmin & 0xffffff80) == 0xffffff80 || (dmin & 0xffffff80) == 0) &&
-             ((dmax & 0xffffff80) == 0xffffff80 || (dmax & 0xffffff80) == 0))
-        byte_leng = 1;
-    else if (((dmin & 0xffff8000) == 0xffff8000 || (dmin & 0xffff8000) == 0) &&
-             ((dmax & 0xffff8000) == 0xffff8000 || (dmax & 0xffff8000) == 0))
-        byte_leng = 2;
-    else if (((dmin & 0xff800000) == 0xff800000 || (dmin & 0xff800000) == 0) &&
-             ((dmax & 0xff800000) == 0xff800000 || (dmax & 0xff800000) == 0))
-        byte_leng = 3;
-    else
-        byte_leng = 4;
-    /* make a 4 byte long header */
-    buf = outbuf;
-    *buf++ = (sys_ch >> 8) & 0xff;
-    *buf++ = sys_ch & 0xff;
-    *buf++ = (byte_leng << 4) | (sr >> 8);
-    *buf++ = sr & 0xff;
-
-    /* first sample is always 4 byte long */
-    *buf++ = inbuf[0] >> 24;
-    *buf++ = inbuf[0] >> 16;
-    *buf++ = inbuf[0] >> 8;
-    *buf++ = inbuf[0];
-    /* second and after */
-    switch (byte_leng) {
-    case 0:
-        for (i = 1; i < sr - 1; i += 2)
-            *buf++ = (inbuf[i] << 4) | (inbuf[i + 1] & 0xf);
-        if (i == sr - 1)
-            *buf++ = (inbuf[i] << 4);
-        break;
-    case 1:
-        for (i = 1; i < sr; i++)
-            *buf++ = inbuf[i];
-        break;
-    case 2:
-        for (i = 1; i < sr; i++) {
-            *buf++ = inbuf[i] >> 8;
-            *buf++ = inbuf[i];
-        }
-        break;
-    case 3:
-        for (i = 1; i < sr; i++) {
-            *buf++ = inbuf[i] >> 16;
-            *buf++ = inbuf[i] >> 8;
-            *buf++ = inbuf[i];
-        }
-        break;
-    case 4:
-        for (i = 1; i < sr; i++) {
-            *buf++ = inbuf[i] >> 24;
-            *buf++ = inbuf[i] >> 16;
-            *buf++ = inbuf[i] >> 8;
-            *buf++ = inbuf[i];
-        }
-        break;
-    }
-    return ((int) (buf - outbuf));
 }

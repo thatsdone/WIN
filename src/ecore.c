c9dad93a2b20f1b678494596933b9e052663e710
uehira <uehira@bosai.go.jp>
2010-10-09 01:42:10 +0900

    * src/ecore.c : 64bit clean? & fixed bugs.

--- src/ecore.c
+++ src/ecore.c
@@ -1,4 +1,4 @@
-/* $Id: ecore.c,v 1.4.4.2.2.6 2010/09/20 03:33:27 uehira Exp $ */
+/* $Id: ecore.c,v 1.4.4.2.2.7 2010/10/08 16:42:10 uehira Exp $ */
 
 /* ddr news program "ecore.c"
   "ecore.c" works with "fromtape.c"
@@ -14,18 +14,20 @@
 	output file:YYMMDDHH.mm...win format
 */
 /*  03/04/25 for Endian free and fix by N.Nakakawaji */
+/*  2010/10/08 64bit clean? fixed bug (Uehira) */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
+#include  <sys/types.h>
+#include  <sys/file.h>
+#include  <sys/stat.h>
+
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <string.h>
 #include  <unistd.h>
-#include  <sys/file.h>
-#include  <sys/types.h>
-#include  <sys/stat.h>
 #include  <fcntl.h>
 
 #include "winlib.h"
@@ -52,16 +54,19 @@
 /*#define   NAMLEN    80*/
 #define   NAMLEN   255 
 
+static const char rcsid[] = 
+  "$Id: ecore.c,v 1.4.4.2.2.7 2010/10/08 16:42:10 uehira Exp $";
+
 /* not use out_data 03/03/07 */
 /*short out_data[SR*NCH];*/
 /* change size 100*1000=> 500*1000 03/02/28 */
 /*unsigned char in_data[100*1000]; */
-unsigned char in_data[500*1000];
-int pos_table[CHMASK+1],f_out,f_in;
-char path_raw[NAMLEN],file_written[NAMLEN],file_done[NAMLEN],
-  file_list[NAMLEN],file_out[NAMLEN],dir_out[NAMLEN],
+static uint8_w in_data[500*1000];
+static int pos_table[CHMASK+1],f_out,f_in;
+static char *path_raw,file_written[NAMLEN],file_done[NAMLEN],
+  *file_list,file_out[NAMLEN],dir_out[NAMLEN],
   name_file[NAMLEN],time_done[NAMLEN];
-FILE *fp;
+static FILE *fp;
 
 struct {
   int sys_ch;
@@ -71,7 +76,14 @@ struct {
   double gn_filt;
   double coef[MAX_FILT*4];
   double uv[MAX_FILT*4];
-  } ch[NCH];
+  } static ch[NCH];
+
+/* prototypes */
+static void end_process(int);
+static void bfov_error(void);
+static WIN_sr get_data(uint8_w **, int32_w *, int);
+int main(int, char *[]);
+
 /*****  deleted for Endian Free  03/04/25
 mklong(ptr)
         unsigned char *ptr;
@@ -107,9 +119,10 @@ mkshort(ptr)
 /*     dest[cntr]=((sour[cntr]>>4)&0xf)*10+(sour[cntr]&0xf); */
 /*   } */
 
-end_process(value)
-  int value;
+static void
+end_process(int value)
   {
+
   if(f_in!=-1) close(f_in);
   if(f_out!=-1)
     {
@@ -122,27 +135,41 @@ end_process(value)
   exit(value);
   }
 
-main(argc,argv)
-  int argc;
-  char *argv[];
+static void
+bfov_error()
+{
+
+  fprintf(stderr, "Buffer overrun!\n");
+  end_process(1);
+}
+
+
+int
+main(int argc, char *argv[])
   {
   double buf[SSR],dt;
-  long lbuf[SSR],dl;
-  unsigned char *ptr,*ptr_end,textbuf[NAMLEN];
+  int32_w lbuf[SSR],dl;
+  uint8_w *ptr,*ptr_end;
+  char  textbuf[NAMLEN];
   int dec_start[6],dec_end[6],dec_wtn[6],dec_now[6],dec_buf[6],
-    i,j,nch,next,sys_ch,sr,pos,size,cnt_min;
-  //int fildata[SSR];   int => long 03/04/23
+    i,j,nch,next,sys_ch,pos,cnt_min;
+  /* int fildata[SSR];   int => long 03/04/23 */
+  WIN_sr  sr;
+  uint32_w  size;
   uint8_w sizetmp[4];
   int32_w fildata[SSR];
-  unsigned char windata[500*1000],*wptr;  /* added 03/03/07 */
-  int idx,bsize;  				  /* added 03/03/07 */
-  unsigned char min_head[6];  		  /* added 03/03/07 */
+  uint8_w windata[500*1000],*wptr;  /* added 03/03/07 */
+  /* WIN_bs idx;  				  /\* added 03/03/07 *\/ */
+  uint32_w  bsize;
+  uint8_w min_head[6];  		  /* added 03/03/07 */
   uint8_w c_bsize[4];			/* added 03/04/22 */
 
   f_in=f_out=(-1);
   printf("***** ecore start *****\n");
   if(argc<5)
     {
+    WIN_version();
+    printf("%s\n", rcsid);
     printf("usage of 'ecore' :\n");
     printf(" 'ecore [YYMMDD.HHMM(1)] [YYMMDD.HHMM(2)] [raw dir] [ch list] ([out dir])'\n");
     printf("    ('ecore' cooperates with 'fromtape')\n");
@@ -166,13 +193,17 @@ main(argc,argv)
     dec_end[3],dec_end[4],dec_end[5]);
 
   /* read input directory */
-  sscanf(argv[3],"%s",path_raw);
+  path_raw = argv[3];
   printf("input directory (raw) : %s\n",path_raw);
-  sprintf(file_written,"%s/%s",path_raw,WDISK_LATEST); /* or FROMTAPE_LATEST */
-  sprintf(file_done,"%s/%s",path_raw,ECORE_USED);
+  if (snprintf(file_written, sizeof(file_written), "%s/%s", path_raw,
+	       WDISK_LATEST) >= sizeof(file_written)) /* or FROMTAPE_LATEST */
+    bfov_error();
+  if (snprintf(file_done, sizeof(file_done), "%s/%s",
+	       path_raw, ECORE_USED) >= sizeof(file_done))
+    bfov_error();
 
   /* read channel list */
-  sscanf(argv[4],"%s",file_list);
+  file_list = argv[4];
   if((fp=fopen(file_list,"r"))==NULL)
     {
     printf("channel list file '%s' not open\n",file_list);
@@ -224,10 +255,10 @@ main(argc,argv)
     fscanf(fp,"%02d%02d%02d%02d.%02d",&dec_wtn[0],&dec_wtn[1],
       &dec_wtn[2],&dec_wtn[3],&dec_wtn[4]);
     fclose(fp);
-//printf("next   =%02d%02d%02d%02d.%02d\n",dec_now[0],dec_now[1],dec_now[2],
-//  dec_now[3],dec_now[4]);
-//printf("written=%02d%02d%02d%02d.%02d\n",dec_wtn[0],dec_wtn[1],dec_wtn[2],
-//  dec_wtn[3],dec_wtn[4]);
+/* //printf("next   =%02d%02d%02d%02d.%02d\n",dec_now[0],dec_now[1],dec_now[2], */
+/* //  dec_now[3],dec_now[4]); */
+/* //printf("written=%02d%02d%02d%02d.%02d\n",dec_wtn[0],dec_wtn[1],dec_wtn[2], */
+/* //  dec_wtn[3],dec_wtn[4]); */
     if(time_cmp(dec_wtn,dec_now,6)<0)
       {
       printf("waiting '%s' to be updated ...\n",file_written);
@@ -235,29 +266,33 @@ main(argc,argv)
       continue;
       }
     /* file ready */
-    sprintf(name_file,"%s/%02d%02d%02d%02d.%02d",path_raw,
-      dec_now[0],dec_now[1],dec_now[2],dec_now[3],dec_now[4]);
-    sprintf(time_done,"%02d%02d%02d%02d.%02d",
-      dec_now[0],dec_now[1],dec_now[2],dec_now[3],dec_now[4]);
+    if (snprintf(name_file, sizeof(name_file),"%s/%02d%02d%02d%02d.%02d",
+		 path_raw,  dec_now[0], dec_now[1],
+		 dec_now[2], dec_now[3], dec_now[4]) >= sizeof(name_file))
+      bfov_error();
+    if (snprintf(time_done, sizeof(time_done), "%02d%02d%02d%02d.%02d",
+		 dec_now[0], dec_now[1], dec_now[2],
+		 dec_now[3], dec_now[4]) >= sizeof(time_done))
+      bfov_error();
     if((f_in=open(name_file,O_RDONLY))==-1)
       {
       printf("no file : %s\n",name_file);
 
-      //------------ no input file => no output file 03/03/12 
-      //for(i=0;i<60;i++)
-      //  {
-      //  /* output dummy */
-      //  for(j=0;j<sizeof(windata);j++) windata[j]=0;
-      //  /*if(write(f_out,(char *)out_data,h)==(-1))*/ /*03/03/07 */
-      //  if(write(f_out,(char *)windata,sizeof(windata))==(-1))
-      //    {
-      //    perror("write");
-      //    end_process(1);
-      //    }
-      //  dec_now[5]++;
-      //  adj_time(dec_now);
-      //  }
-      //-------------------------------------------------------*/
+      /* /\*\//------------ no input file => no output file 03/03/12  */
+      /* //for(i=0;i<60;i++) */
+      /* //  { */
+      /* //  /\* output dummy *\/ */
+      /* //  for(j=0;j<sizeof(windata);j++) windata[j]=0; */
+      /* //  /\*if(write(f_out,(char *)out_data,h)==(-1))*\/ /\*03/03/07 *\/ */
+      /* //  if(write(f_out,(char *)windata,sizeof(windata))==(-1)) */
+      /* //    { */
+      /* //    perror("write"); */
+      /* //    end_process(1); */
+      /* //    } */
+      /* //  dec_now[5]++; */
+      /* //  adj_time(dec_now); */
+      /* //  } */
+      /* //-------------------------------------------------------*\/ */
       dec_now[4]++;
       adj_time(dec_now);
       continue;  /* return loop */
@@ -265,10 +300,12 @@ main(argc,argv)
     else
       {
       printf("'%s' opened\n",name_file);
-  	fflush(stdout); /* 03/03/07 */
+      fflush(stdout); /* 03/03/07 */
       /* out file 03/03/07 */
-      sprintf(file_out,"%s/%02d%02d%02d%02d.%02d",dir_out,
-        dec_now[0],dec_now[1],dec_now[2],dec_now[3],dec_now[4]);
+      if (snprintf(file_out, sizeof(file_out), "%s/%02d%02d%02d%02d.%02d",
+		   dir_out, dec_now[0], dec_now[1],
+		   dec_now[2],dec_now[3],dec_now[4]) >= sizeof(file_out))
+	bfov_error();
       if((f_out=open(file_out,O_RDWR|O_CREAT|O_TRUNC,0666))==-1)
         {
         printf("file open error : %s\n",file_out);
@@ -279,21 +316,23 @@ main(argc,argv)
       for(i=0;i<60;i++)
         {
         /* added 2lines 03/03/07 */
-        idx = 0;
+	/* idx = 0; */
         wptr = windata;
         /*for(j=0;j<SR*nch;j++) out_data[j]=0;*/ /* deleted 03/03/07 */
-        for(j=0;j<sizeof(windata);j++) windata[j]=0;
+        /* for(j=0;j<sizeof(windata);j++) windata[j]=0; */
+	memset(windata, 0, sizeof(windata));
         /* read one sec data */
-        if(read(f_in,sizetmp,4)<=0)
+        if(read(f_in,sizetmp,4)!=4)
           {
           perror("read");
           break;
           }
 	size=mkuint4(sizetmp);  /* for Endian free  03/04/25 */
-
-	//printf("size=%d(%x)\n",size,size);	/* 030228 */
-	//fflush(stdout);
-        if(read(f_in,in_data,size-4)<=0)
+	if (sizeof(in_data) < size - 4)
+	  bfov_error();
+	/* //printf("size=%d(%x)\n",size,size);	/\* 030228 *\/ */
+	/* //fflush(stdout); */
+        if(read(f_in,in_data,size-4)!=size-4)
           {
           perror("read");
           break;
@@ -302,13 +341,13 @@ main(argc,argv)
         /* min_header save 03/03/07 */
         for(j=0;j<6;j++)
 		min_head[j]=in_data[j];
-//	printf("in_data:");
-//	for(j=0;j<6;j++)
-//		printf("%d ",in_data[j]);
-//	printf("\ndec_buf:");
-//	for(j=0;j<6;j++)
-//		printf("%d ",dec_buf[j]);
-//	printf("\n");
+/* //	printf("in_data:"); */
+/* //	for(j=0;j<6;j++) */
+/* //		printf("%d ",in_data[j]); */
+/* //	printf("\ndec_buf:"); */
+/* //	for(j=0;j<6;j++) */
+/* //		printf("%d ",dec_buf[j]); */
+/* //	printf("\n"); */
 
         /* check time of data */
         next=1;
@@ -354,9 +393,9 @@ main(argc,argv)
               for(j=0;j<SR;j++)
                 {
 #if FILTER
-                dl=(long)(buf[(j*sr)/SR]*ch[pos].gn_filt);
+                dl=(int32_w)(buf[(j*sr)/SR]*ch[pos].gn_filt);
 #else
-                dl=(long)buf[(j*sr)/SR];
+                dl=(int32_w)buf[(j*sr)/SR];
 #endif
 		/* deleted 2lines 03/03/07 */
                 /*if(dl<(-32768)) dl=(-32768);*/
@@ -364,15 +403,15 @@ main(argc,argv)
 		/* out_data => 1sec data  03/03/07 */
                 /*out_data[j*nch+pos]=dl;*/
                 fildata[j]=dl;		/* filtering data => fildata 03/03/07 */
-		//printf("fildata[%d]=%x\n",j,fildata[j]);	/* /03/04/25 */
-		//fflush(stdout);
+		/* //printf("fildata[%d]=%x\n",j,fildata[j]);	/\* /03/04/25 *\/ */
+		/* //fflush(stdout); */
                 }
 	      /* data => channel block by win format 03/03/07 */
-		wptr=wptr+idx;
-		//idx = winform((long *)fildata,wptr,SR,(short)sys_ch); int=>long 03/04/23 
-		idx = winform(fildata,wptr,SR,(short)sys_ch);
-		//printf("winform:idx=%d,sys_ch=%x\n",idx,sys_ch);  /* 03/04/23 */
-		//fflush(stdout);
+		/* //idx = winform((long *)fildata,wptr,SR,(short)sys_ch); int=>long 03/04/23  */
+		wptr += winform(fildata,wptr,SR,(WIN_ch)sys_ch);
+		/* wptr=wptr+idx; */
+		/* //printf("winform:idx=%d,sys_ch=%x\n",idx,sys_ch);  /\* 03/04/23 *\/ */
+		/* //fflush(stdout); */
               }
             break;
           case 1:     /* not to write */
@@ -385,7 +424,7 @@ main(argc,argv)
         if(next)      /* write data => win format 03/03/07 */
           {
 	  /* write block size */
-          bsize = wptr - windata + 10;
+	    bsize = (uint32_w)(wptr - windata + 10);
 	  //printf("output bsize:%d(%x)\n",bsize,bsize); /* 03/03/14 */
 	  //fflush(stdout);
 	  c_bsize[0] = bsize >>24;
@@ -393,15 +432,15 @@ main(argc,argv)
 	  c_bsize[2] = bsize >>8;
 	  c_bsize[3] = bsize;
 	  /* c_bsize=mkuint4(&bsize);	/\* added 03/04/22 *\/ */
-	  //printf("output c_bsize:%d(%x)\n",c_bsize,c_bsize); /* 03/03/22 */
-	  //fflush(stdout);
-          if(write(f_out,c_bsize,4)==(-1))
+	  /* //printf("output c_bsize:%d(%x)\n",c_bsize,c_bsize); /\* 03/03/22 *\/ */
+	  /* //fflush(stdout); */
+          if(write(f_out,c_bsize,4) != 4)
             {
             perror("fprintf");
             end_process(1);
             }
 	  /* write min header */
-          if(write(f_out,min_head,6)==(-1))
+          if(write(f_out,min_head,6) != 6)
             {
             perror("write");
             end_process(1);
@@ -409,7 +448,7 @@ main(argc,argv)
 	
 	  /* write channel blocks */
           /*if(write(f_out,(char *)out_data,SR*2*nch)==(-1))*/ /*03/03/07 */
-          if(write(f_out,(char *)windata,bsize-10)==(-1))
+          if(write(f_out,windata,bsize-10) != bsize-10)
             {
             perror("write");
             end_process(1);
@@ -432,8 +471,10 @@ main(argc,argv)
       {
       cnt_min=0;
       close(f_out);
-      sprintf(file_out,"%s/%02d%02d%02d.%02d%02d",dir_out,
-        dec_now[0],dec_now[1],dec_now[2],dec_now[3],dec_now[4]);
+      if (snprintf(file_out, sizeof(file_out), "%s/%02d%02d%02d.%02d%02d",
+		   dir_out, dec_now[0], dec_now[1], dec_now[2],
+		   dec_now[3],dec_now[4]) >= sizeof(file_out))
+	bfov_error();
       if((f_out=open(file_out,O_RDWR|O_CREAT|O_TRUNC,0666))==-1)
         {
         printf("file open error : %s\n",file_out);
@@ -446,71 +487,89 @@ main(argc,argv)
   end_process(0);
   }
 
-get_data(dp,buf,idx)
-  unsigned char **dp;
-  long *buf;
-  int idx;
+static WIN_sr
+get_data(uint8_w **dp, int32_w *buf, int idx)
   {
-  unsigned char *ddp;
-  int gh,s_rate,g_size,sys_ch,i,b_size;
-  short shreg;		/* added 03/05/02 */
+  uint8_w *ddp;
+  WIN_sr  s_rate;
+  WIN_ch  sys_ch;
+  WIN_bs  g_size;
+  /* int gh,i,b_size; */
+  /* short shreg;	 */	/* added 03/05/02 */
+
   ddp=(*dp);
-  s_rate=(gh=mkuint4(ddp))&0xfff;
-  ddp+=4;
-  if((b_size=(gh>>12)&0xf)) g_size=b_size*(s_rate-1)+4;
-  else g_size=(s_rate>>1)+4;
-  *dp+=4+g_size;
-  sys_ch=gh>>16;
-  if(idx==0) return(s_rate);
+  g_size = win_get_chhdr(ddp, &sys_ch, &s_rate);
+  *dp += g_size;
+#if DEBUG
+  printf("ecore(get_data):idx=%d,s_rate=%d g_size=%u, sys_ch=%04X\n",
+	 idx,s_rate,g_size,sys_ch);
+  fflush(stdout);
+#endif
+  if (idx == 0)
+    return(s_rate);
+
+  g_size = win2fix(ddp, buf, &sys_ch, &s_rate);
+  if (g_size == 0)
+    return (-1);
+  else
+    return(s_rate);
+
+  /* s_rate=(gh=mkuint4(ddp))&0xfff; */
+  /* ddp+=4; */
+  /* if((b_size=(gh>>12)&0xf)) g_size=b_size*(s_rate-1)+4; */
+  /* else g_size=(s_rate>>1)+4; */
+  /* *dp+=4+g_size; */
+  /* sys_ch=gh>>16; */
+  /* if(idx==0) return(s_rate); */
 
   //printf("ecore(get_data):idx=%d,b_size=%x, gh=%x, g_size=%x, sys_ch=%x\n",idx,b_size,gh,g_size,sys_ch); /* 03/04/25 */
-  fflush(stdout);
+  /* fflush(stdout); */
   /* read group */
-  buf[0]=mkuint4(ddp);
-  ddp+=4;
-  switch(b_size)
-    {
-    case 0:
-      for(i=1;i<s_rate;i+=2)
-        {
-        buf[i]=buf[i-1]+((*(char *)ddp)>>4);
-        buf[i+1]=buf[i]+(((char)(*(ddp++)<<4))>>4);
-        }
-      break;
-    case 1:
-      for(i=1;i<s_rate;i++)
-        buf[i]=buf[i-1]+(*(char *)(ddp++));
-      break;
-    case 2:
-      for(i=1;i<s_rate;i++)
-        {
-	/* 2lines edit 03/05/02 N.Nakawaji
-        buf[i]=buf[i-1]+mkuint2(ddp);
-        ddp+=2;
-	*/
-	shreg=((ddp[0]<<8) & 0xff00) + (ddp[1] & 0xff);
-	ddp += 2;
-	buf[i] = buf[i-1] + shreg;
-        }
-      break;
-    case 3:
-      for(i=1;i<s_rate;i++)
-        {
-        buf[i]=buf[i-1]+(mkuint4(ddp)>>8);
-        ddp+=3;
-        }
-      break;
-    case 4:
-      for(i=1;i<s_rate;i++)
-        {
-        buf[i]=buf[i-1]+mkuint4(ddp);
-        ddp+=4;
-        }
-      break;
-    default:
-      return(-1); /* bad header */
-    }
-  /*for(i=0;i<s_rate;i++) printf("%x",buf[i]);*/  /* 03/03/03 */
-  /*printf("\n");*/ /* 03/03/03 */
-  return(s_rate); /* normal return */
+  /* buf[0]=mkuint4(ddp); */
+  /* ddp+=4; */
+  /* switch(b_size) */
+  /*   { */
+  /*   case 0: */
+  /*     for(i=1;i<s_rate;i+=2) */
+  /*       { */
+  /*       buf[i]=buf[i-1]+((*(char *)ddp)>>4); */
+  /*       buf[i+1]=buf[i]+(((char)(*(ddp++)<<4))>>4); */
+  /*       } */
+  /*     break; */
+  /*   case 1: */
+  /*     for(i=1;i<s_rate;i++) */
+  /*       buf[i]=buf[i-1]+(*(char *)(ddp++)); */
+  /*     break; */
+  /*   case 2: */
+  /*     for(i=1;i<s_rate;i++) */
+  /*       { */
+  /* 	/\* 2lines edit 03/05/02 N.Nakawaji */
+  /*       buf[i]=buf[i-1]+mkuint2(ddp); */
+  /*       ddp+=2; */
+  /* 	*\/ */
+  /* 	shreg=((ddp[0]<<8) & 0xff00) + (ddp[1] & 0xff); */
+  /* 	ddp += 2; */
+  /* 	buf[i] = buf[i-1] + shreg; */
+  /*       } */
+  /*     break; */
+  /*   case 3: */
+  /*     for(i=1;i<s_rate;i++) */
+  /*       { */
+  /*       buf[i]=buf[i-1]+(mkuint4(ddp)>>8); */
+  /*       ddp+=3; */
+  /*       } */
+  /*     break; */
+  /*   case 4: */
+  /*     for(i=1;i<s_rate;i++) */
+  /*       { */
+  /*       buf[i]=buf[i-1]+mkuint4(ddp); */
+  /*       ddp+=4; */
+  /*       } */
+  /*     break; */
+  /*   default: */
+  /*     return(-1); /\* bad header *\/ */
+  /*   } */
+  /* /\*for(i=0;i<s_rate;i++) printf("%x",buf[i]);*\/  /\* 03/03/03 *\/ */
+  /* /\*printf("\n");*\/ /\* 03/03/03 *\/ */
+  /* return(s_rate); /\* normal return *\/ */
   }

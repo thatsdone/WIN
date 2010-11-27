65efb4dd6323031ebdf10c11668f2fbf2f70947a
uehira <uehira@bosai.go.jp>
2010-11-27 18:37:18 +0900

    src/win.c : update WIN_VERSION.

--- src/win.c
+++ src/win.c
@@ -3,7 +3,7 @@
 * 90.6.9 -      (C) Urabe Taku / All Rights Reserved.           *
 ****************************************************************/
 /* 
-   $Id: win.c,v 1.46.2.6.2.35 2010/09/27 07:53:55 uehira Exp $
+   $Id: win.c,v 1.46.2.6.2.36 2010/11/27 09:37:19 uehira Exp $
 
    High Samping rate
      9/12/96 read_one_sec 
@@ -23,10 +23,10 @@
 #else
 #define NAME_PRG      "win32"
 #endif
-#define WIN_VERSION   "2010.9.27(+Hi-net) 64bit"
+#define WIN_VERSION   "2010.11.27(+Hi-net)"
 
 static const char rcsid[] =
-  "$Id: win.c,v 1.46.2.6.2.35 2010/09/27 07:53:55 uehira Exp $";
+  "$Id: win.c,v 1.46.2.6.2.36 2010/11/27 09:37:19 uehira Exp $";
 
 #define DEBUG_AP      0   /* for debugging auto-pick */
 /* 5:sr, 4:ch, 3:sec, 2:find_pick, 1:all */

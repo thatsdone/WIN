f12b93ec7281ca8084d95030f99f0890e02fd677
uehira <uehira@bosai.go.jp>
2010-12-28 11:41:23 +0900

    bump version.

--- src/win.c
+++ src/win.c
@@ -3,7 +3,7 @@
 * 90.6.9 -      (C) Urabe Taku / All Rights Reserved.           *
 ****************************************************************/
 /* 
-   $Id: win.c,v 1.56 2010/12/28 02:40:36 uehira Exp $
+   $Id: win.c,v 1.57 2010/12/28 02:41:23 uehira Exp $
 
    High Samping rate
      9/12/96 read_one_sec 
@@ -21,7 +21,7 @@
 #else
 #define NAME_PRG      "win32"
 #endif
-#define WIN_VERSION   "2010.12.24(+Hi-net)"
+#define WIN_VERSION   "2010.12.28(+Hi-net)"
 #define DEBUG_AP      0   /* for debugging auto-pick */
 /* 5:sr, 4:ch, 3:sec, 2:find_pick, 1:all */
 /************ HOW TO COMPILE THE PROGRAM **************************
